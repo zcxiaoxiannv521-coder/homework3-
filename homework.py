733c1063019f9e06a1c804b806a85dfe969dcce8
玛卡巴卡 <zcxiaoxiannv521@gmail.com>
2026-06-18 21:24:05 +0800

    "feat: 完成任务2时间分布分析"

--- homework.py
+++ homework.py
@@ -37,3 +37,55 @@ else:
     print(missing_count)
 #执行删除缺失值（如果无缺失，这行不会改动数据）
 df = df.dropna()
+
+#与下一任务分隔
+print()
+
+# 导入绘图库matplotlib，用于后续可视化（本段代码暂未绘图，预留绘图工具）
+import matplotlib.pyplot as plt
+# 筛选刷卡类型等于0的所有数据，单独存入子数据集df0
+df0 = df[df['刷卡类型'] == 0]
+# 获取刷卡类型为0的总乘车记录条数
+total = len(df0)
+# 统计早峰前出行人数：hour<7 即7点前乘车的记录数量
+early = np.sum(df0['hour'] < 7)
+early0=early/total*100
+# 统计深夜出行人数：hour>=22 即22点及之后乘车的记录数量
+late = np.sum(df0['hour'] >= 22)
+late0=late/total*100
+print("[任务2(a)]早峰前/深夜上车刷卡量：")
+# 打印7点前早峰前出行总人次
+print(f"早上7点前公共交通上车刷卡量为：{early}次，占全天{early0:.2f}%")
+# 打印22点后深夜出行总人次
+print(f"晚上10点后公共交通上车刷卡量为：{late}次，占全天{late0:.2f}%")
+# 统计刷卡类型0各小时乘车人次，按0~23小时升序排列
+# value_counts()统计每个hour出现次数；sort_index()按小时0-23从小到大排序
+hour_counts = df0['hour'].value_counts().sort_index()
+# 创建画布，设置尺寸 宽10英寸，高5英寸
+plt.figure(figsize=(10,5))
+# 遍历所有小时，自定义柱子颜色：
+# 小时<7 或 >=22 → 橙色（代表早峰前、深夜时段）；其余白天时段浅蓝色
+colors = ['orange' if (h < 7 or h >= 22) else 'skyblue' for h in hour_counts.index]
+# 绘制柱状图：x轴小时，y轴对应乘车人次，使用上面自定义的区分颜色
+plt.bar(hour_counts.index, hour_counts.values, color=colors)
+# 设置图表标题、坐标轴标签
+plt.title("24-hour Boarding Counts Distribution")
+plt.xlabel("Hour(0~23)")
+plt.ylabel("Boarding Count")
+#准备图例
+plt.bar(hour_counts.index,
+        hour_counts.values,
+        color=colors,
+        label='Early & Late Highlight')
+
+plt.legend()
+# X轴刻度，间隔2小时显示，避免刻度拥挤
+plt.xticks(range(0,24,2))
+# 添加x，y轴网格线，方便读取数值
+plt.grid(axis='y', linestyle='--', alpha=0.5)
+plt.grid(axis='x', linestyle='-', alpha=0.5)
+# 保存图片到代码同目录，分辨率150dpi
+plt.savefig("hour_distribution.png", dpi=150)
+print("[任务2(b)]已保存图像：hour_distribution.png")
+# 弹出窗口展示图表
+plt.show()
